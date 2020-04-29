78775baa41c1dab3c7c804884a339a8953c3b316
Godbobo <980742324@qq.com>
2020-04-29 17:48:53 +0800

    取消点击添加新星球功能

--- js/universe.js
+++ js/universe.js
@@ -10,69 +10,75 @@
  * 
  */
 
-Universe = new function() {
-	
+Universe = new function () {
+
 	// =================
 	// === VARIABLES ===
 	// =================
-	
+
 	var SCREEN_WIDTH = window.innerWidth;
 	var SCREEN_HEIGHT = window.innerHeight;
 	var RADIUS = 70;
 	var FPS = 20;
-	var INITIAL_SATELLITES = 10;
+	var INITIAL_SATELLITES = 20;
 	var INITIAL_PLANETS = 3;
-	
+
 	var mouseX = SCREEN_WIDTH / 2;
 	var mouseY = SCREEN_HEIGHT / 2;
 	var target = null;
-	
+
 	var canvas;
 	var planets = [];
 	var satellites = [];
-	
+
 	// Shadows
 	var planetShadow = {
-		    color: 'rgba(100, 120, 230, 1)',
-		    blur: 20, 
-		    fillShadow: true, 
-		    strokeShadow: true 
-		}
+		color: 'rgba(100, 120, 230, 1)',
+		blur: 20,
+		fillShadow: true,
+		strokeShadow: true
+	}
 	var satelliteShadow = {
-		    color: 'rgba(255, 255, 255, 1)',
-		    blur: 10,
-		    fillShadow: true, 
-		    strokeShadow: true 
-		}
-	
+		color: 'rgba(255, 255, 255, 1)',
+		blur: 10,
+		fillShadow: true,
+		strokeShadow: true
+	}
+
 	// ================================
 	// === INITIALIZATION FUNCTIONS ===
 	// ================================
-	
-	this.init = function() {
+
+	this.init = function () {
 		canvas = new fabric.StaticCanvas("universe", {
 			backgroundColor: '#121212'
 		});
-		
-		// Event listeners
+
+		// 事件监听
 		window.addEventListener('mousemove', documentMouseMoveHandler, false);
-	    window.addEventListener('mousedown', documentMouseDownHandler, false);
-	    window.addEventListener('mouseup', documentMouseUpHandler, false);
-	    window.addEventListener('resize', windowResizeHandler, false);
-	    
-	    // Canvas size
+		window.addEventListener('mousedown', documentMouseDownHandler, false);
+		window.addEventListener('mouseup', documentMouseUpHandler, false);
+		window.addEventListener('resize', windowResizeHandler, false);
+
+		// 设置画布大小
 		canvas.setWidth(SCREEN_WIDTH);
 		canvas.setHeight(SCREEN_HEIGHT);
-		
-		// Initialization
+
+		// 初始化
 		initPlanets();
 		initSatellites();
-      	windowResizeHandler();
-      	setInterval(loop, FPS); // Start loop
+		windowResizeHandler();
+		setInterval(loop, FPS);
 	}
-	
-	function initSatellites() {		
-		for(var i = 0; i < INITIAL_SATELLITES; i++) {
+
+	// 卫星初始化
+	function initSatellites() {
+		for (var i = 0; i < INITIAL_SATELLITES; i++) {
+			// 前三分之一的卫星平均分配到每个星球上
+			var planetIndex = Math.floor(Math.random() * planets.length)
+			if (i < INITIAL_SATELLITES / 3) {
+				planetIndex = i % INITIAL_PLANETS
+			}
 			var s = new fabric.Circle({
 				fill: satelliteShadow.color,
 				radius: 0.5 + Math.random() * 2,
@@ -82,17 +88,19 @@ Universe = new function() {
 				mov: { x: mouseX, y: mouseY },
 				speed: 0.01 + Math.random() * 0.05, // 0.01 --> global speed
 				orbit: RADIUS * 0.7 + (RADIUS * 0.5 * Math.random()),
-				planet: planets[Math.floor(Math.random() * planets.length)],
+				planet: planets[planetIndex],
 			});
 			s.setShadow(satelliteShadow);
-			
-		    canvas.add(s);
-      		satellites.push(s);
+
+			canvas.add(s);
+			satellites.push(s);
 		}
+		console.log(satellites)
 	}
-	
-	function initPlanets() {	
-		for(var i = 0; i < INITIAL_PLANETS; i++) {
+
+	// 星球初始化
+	function initPlanets() {
+		for (var i = 0; i < INITIAL_PLANETS; i++) {
 			var p = new fabric.Circle({
 				fill: planetShadow.color,
 				radius: 10,
@@ -100,14 +108,15 @@ Universe = new function() {
 				top: Math.random() * SCREEN_HEIGHT
 			});
 			p.setShadow(planetShadow);
-			
+
 			canvas.add(p);
 			planets.push(p);
 		}
 	}
-	
+
+	// 创建星球
 	function createPlanet(x, y) {
-		
+
 		// Planet creation
 		var p = new fabric.Circle({
 			fill: planetShadow.color,
@@ -116,13 +125,13 @@ Universe = new function() {
 			top: y
 		});
 		p.setShadow(planetShadow);
-		
+
 		canvas.add(p);
 		planets.push(p);
-		
-		// Adding planet satellites
+
+		// 添加卫星
 		var toAdd = 5;
-		for(var i = 0; i < toAdd; i++) {
+		for (var i = 0; i < toAdd; i++) {
 			var s = new fabric.Circle({
 				fill: "#eaeaea",
 				radius: 0.5 + Math.random() * 2,
@@ -135,127 +144,128 @@ Universe = new function() {
 				planet: p,
 			});
 			s.setShadow(satelliteShadow);
-			
-		    canvas.add(s);
-      		satellites.push(s);
+
+			canvas.add(s);
+			satellites.push(s);
 		}
 	}
-	
+
 	// ======================
 	// === EVENT HANDLERS ===
 	// ======================
-	
+
 	function documentMouseMoveHandler(event) {
 		mouseX = event.clientX - (window.innerWidth - SCREEN_WIDTH) * .5;
 		mouseY = event.clientY - (window.innerHeight - SCREEN_HEIGHT) * .5;
 	}
-	
+
 	function documentMouseDownHandler(event) {
-		for(var i = 0; i < planets.length; i++) {
+		for (var i = 0; i < planets.length; i++) {
 			var p = planets[i];
-			
-			if(event.clientX >= p.left - p.radius - 10 && 
-				event.clientX <= p.left + p.radius + 10 && 
-				event.clientY >= p.top - p.radius - 10 && 
+
+			if (event.clientX >= p.left - p.radius - 10 &&
+				event.clientX <= p.left + p.radius + 10 &&
+				event.clientY >= p.top - p.radius - 10 &&
 				event.clientY <= p.top + p.radius + 10) {
 				target = p;
 				break;
 			}
 		}
-		
-		if(target == null)
+
+		if (target == null)
 			target = "canvas";
 	}
-	
+
 	function documentMouseUpHandler(event) {
 		target = null;
 	}
-	
+
 	function windowResizeHandler() {
 		SCREEN_WIDTH = window.innerWidth;
-    	SCREEN_HEIGHT = window.innerHeight;
-    
-    	canvas.setWidth(SCREEN_WIDTH);
-    	canvas.setHeight(SCREEN_HEIGHT);
+		SCREEN_HEIGHT = window.innerHeight;
+
+		canvas.setWidth(SCREEN_WIDTH);
+		canvas.setHeight(SCREEN_HEIGHT);
 	}
-	
+
 	// =============
 	// === UTILS ===
 	// =============
-	
+
 	function distance(c1, c2) {
 		var x1 = c1.left;
 		var x2 = c2.left;
 		var y1 = c1.top;
 		var y2 = c2.top;
-		
+
 		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
 	}
-	
+
 	function numberOfSatellites(planet) {
 		var count = 0;
-		
-		for(var i = 0; i < satellites.length; i++)
-			if(satellites[i].planet == planet)
+
+		for (var i = 0; i < satellites.length; i++)
+			if (satellites[i].planet == planet)
 				count++;
-		
+
 		return count;
 	}
-	
+
 	function setSize() {
-		for(var i = 0; i < planets.length; i++) {
+		for (var i = 0; i < planets.length; i++) {
 			var p = planets[i];
 			var neighbors = numberOfSatellites(p);
-			
+
 			p.radius += ((neighbors) - p.radius) * 0.025;
-      		p.radius = Math.max(p.radius, 2);
+			p.radius = Math.max(p.radius, 2);
 		}
 	}
-	
+
 	// =================
 	// === MAIN LOOP ===
 	// =================
-	
+
 	function loop() {
-		if(target) {
-			target.left = mouseX;
-			target.top = mouseY;	
-		}
-		
-		if(target == "canvas")
-			createPlanet(mouseX, mouseY);
-		
-		for(var i = 0; i < satellites.length; i++) {
+		// if (target) {
+		// 	target.left = mouseX;
+		// 	target.top = mouseY;
+		// }
+
+		// 关闭点击创建星球的功能
+		// if (target == "canvas")
+		// 	createPlanet(mouseX, mouseY);
+
+		for (var i = 0; i < satellites.length; i++) {
 			var s = satellites[i];
-			
+
 			// Rotation
 			s.offset.x += s.speed;
 			s.offset.y += s.speed;
-			
+
 			// Movement Effect
 			s.mov.x += (s.planet.left - s.mov.x) * s.speed;
 			s.mov.y += (s.planet.top - s.mov.y) * s.speed;
-			
+
 			// Change position
 			s.left = s.mov.x + Math.cos(i + s.offset.x) * (s.orbit);
 			s.top = s.mov.y + Math.sin(i + s.offset.y) * (s.orbit);
-			
+
 			// Check "attraction"
-			for(var j = 0; j < planets.length; j++) {
+			for (var j = 0; j < planets.length; j++) {
 				var p = planets[j];
-				
-				if(s.planet != p) {
+
+				if (s.planet != p) {
 					var d1 = distance(p, s);
 					var d2 = distance(s.planet, s);
-					
-					if(d1 < d2)
+
+					if (d1 < d2)
 						s.planet = p;
 				}
 			}
 		}
-		
+
 		setSize();
-		
+
 		canvas.renderAll();
 	}
 };
